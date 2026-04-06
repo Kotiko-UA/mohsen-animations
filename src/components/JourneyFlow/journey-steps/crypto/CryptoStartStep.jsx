6cc120e5e7e29d9ada6ecb8f3cc4c6553b2abe95
kotiko <megasuslik666@gmail.com>
2026-04-06 15:33:59 +0300

    add start modal

--- src/components/JourneyFlow/journey-steps/crypto/CryptoStartStep.jsx
+++ src/components/JourneyFlow/journey-steps/crypto/CryptoStartStep.jsx
@@ -26,12 +26,32 @@ export default function CryptoStartStep() {
 			</p>
 
 			<div className='journey-first-step-bw'>
-				<JourneyNextButton className='journey-modal-main-button' />
+				<JourneyNextButton className='journey-modal-main-button'>
+					Continue
+				</JourneyNextButton>
 				<p className='journey-modal-text'>
 					<span>+2,500</span> already enrolled
 				</p>
 			</div>
-			<div></div>
+			<div className='journey-modal-fs-subblock'>
+				<div className='journey-modal-fs-you-get'>You get:</div>
+				<div className='journey-modal-fs-sb-w'>
+					<div className='journey-modal-fs-sb-1'>
+						<div className='journey-modal-fs-sb-1-top'>
+							<span>6</span>
+							<p>comprehensive sections</p>
+						</div>
+						<p className='journey-modal-fs-sb-1-bottom'>
+							Learn Cryptocurrencies & Enter the Market
+						</p>
+					</div>
+					<ul className='journey-modal-fs-sb-2'>
+						<li>6-12 months</li>
+						<li>Flexible schedule</li>
+						<li>Beginner | Intermediate | Advanced</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	)
 }
