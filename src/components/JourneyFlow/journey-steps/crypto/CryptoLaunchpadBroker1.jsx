f037c7cad336356ec0d654ae1aa0c958de934e42
kotiko <megasuslik666@gmail.com>
2026-04-22 11:28:03 +0300

    crypto wes basic modal edits

--- src/components/JourneyFlow/journey-steps/crypto/CryptoLaunchpadBroker1.jsx
+++ src/components/JourneyFlow/journey-steps/crypto/CryptoLaunchpadBroker1.jsx
@@ -13,7 +13,7 @@ export default function CryptoLaunchpadBroker1() {
 			</div>
 			<div className='flex-col-24'>
 				<div className='flex-col-12'>
-					<div className='text-h-16-700'>What Happens Next?</div>
+					<div className='text-h-16-700'>Why You’re Seeing This?</div>
 					<p className='journey-modal-text'>
 						We often receive questions like: “Which broker should I choose?” WOW
 						is an education and software platform, so we don’t operate brokers
--- src/components/JourneyFlow/journey-steps/crypto/CryptoOnboardingStep1.jsx
+++ src/components/JourneyFlow/journey-steps/crypto/CryptoOnboardingStep1.jsx
@@ -2,6 +2,7 @@ import {
 	JourneyLinkButton,
 	JourneyMobileBackToPointsButton,
 } from '../../JourneyStepControls'
+import Img from '../../../../assets/wes-img-1.jpg'
 
 export default function CryptoOnboardingStep1() {
 	return (
@@ -10,15 +11,17 @@ export default function CryptoOnboardingStep1() {
 				<JourneyMobileBackToPointsButton />
 				<h2 className='text-h-32-700'>WOW Education System: Crypto Basics</h2>
 			</div>
-			<p className='journey-modal-text'>
-				<span>Wow Education System (WES) Crypto Program</span> guides you step
-				by step into the world of cryptocurrency and digital assets. The journey
-				begins with the <span>Basic Level</span>, which introduces core concepts
-				such as blockchain fundamentals, cryptocurrencies, wallets, exchanges,
-				and security best practices through short, interactive, and
-				easy-to-understand modules.
-			</p>
-
+			<div className='flex-col-24-40'>
+				<p className='journey-modal-text'>
+					<span>Wow Education System (WES) Crypto Program</span> guides you step
+					by step into the world of cryptocurrency and digital assets. The
+					journey begins with the <span>Basic Level</span>, which introduces
+					core concepts such as blockchain fundamentals, cryptocurrencies,
+					wallets, exchanges, and security best practices through short,
+					interactive, and easy-to-understand modules.
+				</p>
+				<img src={Img} alt='decorative' className='modal-img' />
+			</div>
 			<JourneyLinkButton
 				link={
 					'https://wowpowers.com/catalog/paths/d72c025a-c289-482e-b0ef-0536529da0f9'
